897eaaec92f129b6dcd4c91151ace89da74cfbd2
Enrico <enricofen300@gmail.com>
2024-12-26 13:10:55 +0100

    class model seq2seq now works fine

--- architectures/BaseModel.py
+++ architectures/BaseModel.py
@@ -40,13 +40,6 @@ class BaseModel(ABC):
         self.reverse_source_word_index = self.x_tokenizer.index_word
         self.target_word_index = self.y_tokenizer.word_index
         self.name = name
-        self.encoder_inputs, self.encoder_outputs, self.state_h, self.state_c = (
-            self.build_encoder()
-        )
-        (
-            self.decoder_inputs,
-            self.decoder_outputs,
-        ) = self.build_decoder(self.encoder_outputs, self.state_h, self.state_c)
         self.model = self.build_model()
 
     def get_model(self):
--- architectures/Seq2SeqLSTM.py
+++ architectures/Seq2SeqLSTM.py
@@ -16,20 +16,36 @@ from tensorflow.keras.models import Model
 
 from architectures.BaseModel import BaseModel
 
-# FIXME:
-# devo provare a costruire encoder_model e decoder_model quando faccio l'init dell'istanza della classe
-# e provare a fare inferenza da li
 
 class Seq2SeqLSTM(BaseModel):
     def __init__(
         self, x_voc, y_voc, max_text_len, max_summary_len, x_tokenizer, y_tokenizer
     ):
+        # Set unique parameters for this model
         self.latent_dim = 300
         self.embedding_dim = 100
         self.name = "Seq2SeqLSTM"
         self.reverse_target_word_index = y_tokenizer.index_word
         self.reverse_source_word_index = x_tokenizer.index_word
         self.target_word_index = y_tokenizer.word_index
+
+        # Initialize shared layers specific to this class
+        self.encoder_embedding = Embedding(
+            x_voc, self.embedding_dim, trainable=True, name="encoder_embedding"
+        )
+        self.decoder_embedding = Embedding(
+            y_voc, self.embedding_dim, trainable=True, name="decoder_embedding"
+        )
+        self.encoder_lstm1, self.encoder_lstm2, self.encoder_lstm3 = (
+            self.get_encoder_lstm_layers()
+        )
+        self.decoder_lstm = self.get_decoder_lstm_layer()
+        self.attention_layer = AttentionLayer(name="attention_layer")
+        self.decoder_dense = TimeDistributed(
+            Dense(y_voc, activation="softmax"), name="decoder_dense"
+        )
+
+        # Call the parent class's initializer
         super().__init__(
             x_voc,
             y_voc,
@@ -39,62 +55,41 @@ class Seq2SeqLSTM(BaseModel):
             y_tokenizer=y_tokenizer,
             name=self.name,
         )
+
+        # Build models
+        self.model = self.build_model()
         self.encoder_model, self.decoder_model = self.build_inference()
 
-    def build_encoder(self):
-        """Builds the encoder part of the Seq2Seq model."""
-        encoder_inputs = Input(shape=(self.max_text_len,), name="encoder_inputs")
-
-        # Embedding layer
-        enc_emb = Embedding(
-            self.x_voc, self.embedding_dim, trainable=True, name="encoder_embedding"
-        )(encoder_inputs)
-
-        # Encoder LSTM layers
-        encoder_lstm1 = LSTM(
-            self.latent_dim,
-            return_sequences=True,
-            return_state=True,
-            dropout=0.4,
-            recurrent_dropout=0.4,
-            name="encoder_lstm1",
+    def get_encoder_lstm_layers(self):
+        return (
+            LSTM(
+                self.latent_dim,
+                return_sequences=True,
+                return_state=True,
+                dropout=0.4,
+                recurrent_dropout=0.4,
+                name="encoder_lstm1",
+            ),
+            LSTM(
+                self.latent_dim,
+                return_sequences=True,
+                return_state=True,
+                dropout=0.4,
+                recurrent_dropout=0.4,
+                name="encoder_lstm2",
+            ),
+            LSTM(
+                self.latent_dim,
+                return_sequences=True,
+                return_state=True,
+                dropout=0.4,
+                recurrent_dropout=0.4,
+                name="encoder_lstm3",
+            ),
         )
-        encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
 
-        encoder_lstm2 = LSTM(
-            self.latent_dim,
-            return_sequences=True,
-            return_state=True,
-            dropout=0.4,
-            recurrent_dropout=0.4,
-            name="encoder_lstm2",
-        )
-        encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
-
-        encoder_lstm3 = LSTM(
-            self.latent_dim,
-            return_sequences=True,
-            return_state=True,
-            dropout=0.4,
-            recurrent_dropout=0.4,
-            name="encoder_lstm3",
-        )
-        encoder_outputs, state_h, state_c = encoder_lstm3(encoder_output2)
-
-        return encoder_inputs, encoder_outputs, state_h, state_c
-
-    def build_decoder(self, encoder_outputs, state_h, state_c):
-        """Builds the decoder part of the Seq2Seq model."""
-        decoder_inputs = Input(shape=(None,), name="decoder_inputs")
-
-        # Embedding layer
-        dec_emb_layer = Embedding(
-            self.y_voc, self.embedding_dim, trainable=True, name="decoder_embedding"
-        )
-        dec_emb = dec_emb_layer(decoder_inputs)
-
-        # Decoder LSTM
-        decoder_lstm = LSTM(
+    def get_decoder_lstm_layer(self):
+        return LSTM(
             self.latent_dim,
             return_sequences=True,
             return_state=True,
@@ -102,108 +97,75 @@ class Seq2SeqLSTM(BaseModel):
             recurrent_dropout=0.2,
             name="decoder_lstm",
         )
-        decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state=[state_h, state_c])
 
-        # Attention layer
-        attn_layer = AttentionLayer(name="attention_layer")
-        attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+    def build_encoder(self):
+        encoder_inputs = Input(shape=(self.max_text_len,), name="encoder_inputs")
+        enc_emb = self.encoder_embedding(encoder_inputs)
 
-        # Concatenate attention output and decoder LSTM output
-        decoder_concat_input = Concatenate(axis=-1, name="concat_layer")(
-            [decoder_outputs, attn_out]
+        # LSTM layers
+        encoder_output1, state_h1, state_c1 = self.encoder_lstm1(enc_emb)
+        encoder_output2, state_h2, state_c2 = self.encoder_lstm2(encoder_output1)
+        encoder_outputs, state_h, state_c = self.encoder_lstm3(encoder_output2)
+
+        return encoder_inputs, encoder_outputs, state_h, state_c
+
+    def build_decoder(self, encoder_outputs, encoder_states):
+        decoder_inputs = Input(shape=(None,), name="decoder_inputs")
+        dec_emb = self.decoder_embedding(decoder_inputs)
+
+        decoder_outputs, _, _ = self.decoder_lstm(dec_emb, initial_state=encoder_states)
+
+        attn_out, attn_states = self.attention_layer(
+            [encoder_outputs, decoder_outputs]
         )
+        decoder_concat_input = Concatenate(axis=-1)([decoder_outputs, attn_out])
 
-        # Dense layer
-        decoder_dense = TimeDistributed(
-            Dense(self.y_voc, activation="softmax"), name="decoder_dense"
-        )
-        decoder_outputs = decoder_dense(decoder_concat_input)
+        decoder_outputs = self.decoder_dense(decoder_concat_input)
 
         return decoder_inputs, decoder_outputs
 
     def build_model(self):
-        """Builds the complete Seq2Seq model."""
-        # Build encoder
         encoder_inputs, encoder_outputs, state_h, state_c = self.build_encoder()
+        decoder_inputs, decoder_outputs = self.build_decoder(encoder_outputs, [state_h, state_c])
 
-        # Build decoder
-        decoder_inputs, decoder_outputs = self.build_decoder(
-            encoder_outputs, state_h, state_c
-        )
-
-        # Build the model
         model = Model([encoder_inputs, decoder_inputs], decoder_outputs, name=self.name)
-
-        # Return the model
         return model
 
     def build_inference(self):
-        """Builds the inference models for the encoder and decoder."""
-        # Encoder inference model
-        encoder_model = Model(
-            inputs=self.encoder_inputs,
-            outputs=[
-                self.encoder_outputs,
-                self.state_h,
-                self.state_c,
-            ],
-        )
+        encoder_inputs, encoder_outputs, state_h, state_c = self.build_encoder()
+        encoder_model = Model(encoder_inputs, [encoder_outputs, state_h, state_c])
 
-        # Decoder inference model
         decoder_state_input_h = Input(shape=(self.latent_dim,))
         decoder_state_input_c = Input(shape=(self.latent_dim,))
-        decoder_hidden_state_input = Input(
-            shape=(self.max_text_len, self.latent_dim),
-        )
+        decoder_hidden_state_input = Input(shape=(self.max_text_len, self.latent_dim))
 
-        # Get the embeddings of the decoder sequence
-        dec_emb_layer = Embedding(self.y_voc, self.embedding_dim, trainable=True)
-        dec_emb = dec_emb_layer(self.decoder_inputs)
-
-        # To predict the next word in the sequence, set the initial states to the states from the previous time step
-        decoder_lstm = LSTM(
-            self.latent_dim,
-            return_sequences=True,
-            return_state=True,
-            dropout=0.4,
-            recurrent_dropout=0.2,
-        )
-        decoder_outputs, state_h, state_c = decoder_lstm(
+        decoder_inputs = Input(shape=(None,), name="decoder_inputs")
+        dec_emb = self.decoder_embedding(decoder_inputs)
+        decoder_outputs, state_h, state_c = self.decoder_lstm(
             dec_emb, initial_state=[decoder_state_input_h, decoder_state_input_c]
         )
 
-        # Attention inference
-        attn_layer = AttentionLayer()
-        attn_out, attn_states = attn_layer(
+        attn_out, attn_states = self.attention_layer(
             [decoder_hidden_state_input, decoder_outputs]
         )
         decoder_concat_input = Concatenate(axis=-1)([decoder_outputs, attn_out])
+        decoder_outputs = self.decoder_dense(decoder_concat_input)
 
-        # A dense softmax layer to generate prob dist. over the target vocabulary
-        decoder_dense = TimeDistributed(Dense(self.y_voc, activation="softmax"))
-        decoder_outputs = decoder_dense(decoder_concat_input)
-
-        # Final decoder model
         decoder_model = Model(
-            [self.decoder_inputs]
-            + [
+            [
+                decoder_inputs,
                 decoder_hidden_state_input,
                 decoder_state_input_h,
                 decoder_state_input_c,
             ],
-            [decoder_outputs] + [state_h, state_c],
+            [decoder_outputs, state_h, state_c],
         )
-
-        self.encoder_model = encoder_model
-        self.decoder_model = decoder_model
 
         return encoder_model, decoder_model
 
     def decode_sequence(self, input_seq):
-        """Decodes an input sequence to generate the output sequence."""
         e_out, e_h, e_c = self.encoder_model.predict(input_seq)
 
-        # Generate empty target sequence of length 1.
         target_seq = np.zeros((1, 1))
         target_seq[0, 0] = self.target_word_index["sostok"]
 
@@ -214,7 +176,6 @@ class Seq2SeqLSTM(BaseModel):
                 [target_seq] + [e_out, e_h, e_c]
             )
 
-            # Sample a token
             sampled_token_index = np.argmax(output_tokens[0, -1, :])
             sampled_token = self.reverse_target_word_index[sampled_token_index]
 
@@ -234,17 +195,17 @@ class Seq2SeqLSTM(BaseModel):
         return decoded_sentence
 
     def seq2summary(self, input_seq):
-        newString = ""
-        for i in input_seq:
-            if (
-                i != 0 and i != self.target_word_index["sostok"]
-            ) and i != self.target_word_index["eostok"]:
-                newString = newString + self.reverse_target_word_index[i] + " "
-        return newString
+        return " ".join(
+            [
+                self.reverse_target_word_index[i]
+                for i in input_seq
+                if i != 0
+                and i != self.target_word_index["sostok"]
+                and i != self.target_word_index["eostok"]
+            ]
+        )
 
-    def seq2text(self,input_seq):
-        newString = ""
-        for i in input_seq:
-            if i != 0:
-                newString = newString + self.reverse_source_word_index[i] + " "
-        return newString
+    def seq2text(self, input_seq):
+        return " ".join(
+            [self.reverse_source_word_index[i] for i in input_seq if i != 0]
+        )
